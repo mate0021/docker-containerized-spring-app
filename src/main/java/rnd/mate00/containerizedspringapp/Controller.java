6f7670bb24cb1ed706a3e19c7dbd48cda3dabd42
mate0021 <mami.ext@gmail.com>
2020-10-26 06:20:04 +0100

    Passing property value into application.

--- src/main/java/rnd/mate00/containerizedspringapp/Controller.java
+++ src/main/java/rnd/mate00/containerizedspringapp/Controller.java
@@ -1,13 +1,20 @@
 package rnd.mate00.containerizedspringapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    private String profileName;
+
+    public Controller(@Value("${spring.profiles.active}") String profileName) {
+        this.profileName = profileName;
+    }
+
     @GetMapping("/greet")
     public String greet() {
-        return "Hi from inside of container!";
+        return String.format("Hi from inside of container! Profile set: %s", profileName);
     }
 }
