2b2664dcee1b78da6848e7ec67d8b0e47281a97d
mate0021 <mami.ext@gmail.com>
2020-10-27 07:03:10 +0100

    Setting loglevel from cmd.

--- src/main/java/rnd/mate00/containerizedspringapp/Controller.java
+++ src/main/java/rnd/mate00/containerizedspringapp/Controller.java
@@ -1,11 +1,15 @@
 package rnd.mate00.containerizedspringapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
 
     private String profileName;
 
@@ -15,6 +19,8 @@ public class Controller {
 
     @GetMapping("/greet")
     public String greet() {
+        log.info("Greeting");
+        log.debug("Greeting was called");
         return String.format("Hi from inside of container! Profile set: %s", profileName);
     }
 }
